d57a9f067b51349fa3b55ade5c3bed71670d8293
Mark Aufflick <mark@aufflick.com>
2016-07-03 20:44:52 +1000

    Initial Commit

---
+++ StackSplit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StackSplit
+//
+//  Created by Mark Aufflick on 3/07/2016.
+//  Copyright © 2016 The High Technology Bureau. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StackSplit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StackSplit
+//
+//  Created by Mark Aufflick on 3/07/2016.
+//  Copyright © 2016 The High Technology Bureau. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

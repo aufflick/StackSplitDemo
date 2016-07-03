06fd9f0769309537e82156e9383be8892a2a51e1
Mark Aufflick <mark@aufflick.com>
2016-07-03 22:56:19 +1000

    Basically working

--- StackSplit/ViewController.swift
+++ StackSplit/ViewController.swift
@@ -8,20 +8,109 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+let dragSnapPoints: CGFloat = 200
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class ViewController: NSViewController, NSGestureRecognizerDelegate {
 
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var leftViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var stackView: ResizeCursorStackView!
+    
+    var leftViewWidthAtDragStart: CGFloat = 0
+    var lastXVelocity: CGFloat = 0
+    
+    @IBAction func pan(sender: NSPanGestureRecognizer) {
+        
+        if sender.state == .Began {
+            leftViewWidthAtDragStart = leftViewWidthConstraint.constant
+            stackView.splitDragging = true
+        }
+        
+        if sender.state == .Cancelled {
+            leftViewWidthConstraint.constant = leftViewWidthAtDragStart
+            
+        } else {
+            var dragWidth = max(0, sender.translationInView(self.view).x + leftViewWidthAtDragStart)
+            
+            if sender.state == .Ended {
+                
+                // ensure we never leave the view in a state in between collapsed and min snap width
+                
+                if lastXVelocity < 0 && dragWidth < dragSnapPoints {
+                    dragWidth = 0
+                } else if lastXVelocity > 0 && dragWidth < dragSnapPoints {
+                    dragWidth = dragSnapPoints
+                }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
+            } else {
+                lastXVelocity = sender.velocityInView(self.view).x
+                
+                // don't make user feel they need to drag precisely to the left
+                
+                if lastXVelocity < 0 && dragWidth < 20 {
+                    dragWidth = 0
+                }
+            }
+            
+            leftViewWidthConstraint.constant = dragWidth
+        }
+        
+        // handle common end-state things
+        if sender.state == .Ended || sender.state == .Cancelled {
+
+            stackView.splitDragging = false
         }
     }
 
-
+    func gestureRecognizerShouldBegin(gestureRecognizer: NSGestureRecognizer) -> Bool {
+        let p = gestureRecognizer.locationInView(self.stackView)
+        return stackView.resizeGrabRect.contains(p)
+    }
 }
 
+class ResizeCursorStackView: NSStackView {
+    
+    var resizeGrabRect: CGRect {
+        
+        guard self.views.count >= 2  else {
+            return CGRectZero
+        }
+
+        var rect = self.bounds
+        rect.origin.x = self.views[0].frame.maxX
+        rect.size.width = self.views[1].frame.minX - rect.origin.x
+        return rect
+    }
+    
+    var splitDragging = false {
+        didSet {
+            self.window?.invalidateCursorRectsForView(self)
+        }
+    }
+    
+    override func resetCursorRects() {
+        super.resetCursorRects()
+        
+        guard self.views.count >= 2  else {
+            return
+        }
+
+        let cursor = self.views[0].bounds.size.width > 0 ? NSCursor.resizeLeftRightCursor() : NSCursor.resizeRightCursor()
+
+        if splitDragging {
+            self.addCursorRect(self.bounds, cursor: cursor)
+        } else {
+            self.addCursorRect(self.resizeGrabRect, cursor: cursor)
+        }
+    }
+}
+
+class WhiteView: NSView {
+    
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+        
+        NSColor.whiteColor().setFill()
+        NSBezierPath(rect: dirtyRect).fill()
+    }
+    
+}
